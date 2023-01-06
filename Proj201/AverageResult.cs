938c2fd4d2b621101037acc1a650cf404851a7c6
onerayhan <onerayhan@sabanciuniv.edu>
2023-01-06 15:33:55 +0300

    Add project files.

---
+++ Proj201/AverageResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj201
+{
+    class AverageResult
+    {
+        public string date { get; set; }
+        public double sharpeAverage { get; set; }
+        public double sortinoAverage { get; set; }
+        public double returnToVARAverage { get; set; }
+        public double calmarAverage { get; set; }
+    }
+}
---
+++ Proj201/Observable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj201
+{
+    public interface Observable
+    {
+        void notifyAllObservers();
+        void notifyReset();
+        void notifyAllObserversDataEnd();
+        void attach(string name , Observer observer);
+    }
+}
